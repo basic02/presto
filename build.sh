6bc110d67b90add423ab9f09f2cd74058af8c2e9
jichen0919 <jichen0919@126.com>
2025-01-05 20:32:23 +0800

    adapt for 313

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-scp presto-cache/target/presto-cache-0.275.jar root@ccycloud-1.jiayin.root.comops.site:/opt/presto/plugin/hive-hadoop2/presto-cache-0.275.jar
+./mvnw clean install -DskipTests -pl :presto-cache -am
+#./mvnw clean install -DskipTests -pl :presto-hive-hadoop2 -am  -s ~/.m2/settings.xml.aliyun
+#./mvnw clean install -DskipTests -rf :presto-hive-hadoop2   -s ~/.m2/settings.xml.aliyun
+scp presto-cache/target/presto-cache-0.275.jar root@ccycloud-1.prestopoc.root.comops.site:/opt/presto/plugin/hive-hadoop2/presto-cache-0.275.jar
--- presto-cache/src/main/java/com/facebook/presto/cache/alluxio/ExtendedLocalCacheFileSystem.java
+++ presto-cache/src/main/java/com/facebook/presto/cache/alluxio/ExtendedLocalCacheFileSystem.java
@@ -34,7 +34,6 @@ public class ExtendedLocalCacheFileSystem
             fs.add("file");
         }
         catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 
